7e9177a5d66d72d3ea6d388a24be602dfcb49f5c
moksh <memoksh01@gmail.com>
2026-03-03 19:15:48 +0530

    Initial Secure DevSecOps project

---
+++ hey-app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'moksh_secret'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handleMessage(msg):
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=55001, debug=True)
